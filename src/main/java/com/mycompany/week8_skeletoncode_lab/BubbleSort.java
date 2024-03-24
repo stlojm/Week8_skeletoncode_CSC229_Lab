a96f160fbcc78819c81c262930ba82379f7ea267
stlojm <159178365+stlojm@users.noreply.github.com>
2024-03-24 19:11:05 -0400

    Update BubbleSort.java

--- src/main/java/com/mycompany/week8_skeletoncode_lab/BubbleSort.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/BubbleSort.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.week8_skeletoncode_lab;
 
 /**
@@ -14,10 +10,33 @@ public class BubbleSort {
         int outer, inner, temp;
         for (outer = size - 1; outer > 0; outer--) { // counting down
             for (inner = 0; inner < outer; inner++) { // bubbling up
-                //ToDo 3: complete this algorithm, test it, provide its time complexity
+                if (a[inner] > a[inner + 1]) { // if current element is greater than the next one
+                    // swap them
+                    temp = a[inner];
+                    a[inner] = a[inner + 1];
+                    a[inner + 1] = temp;
+                }
             }
         }
     }
+
+    public static void main(String[] args) {
+        int[] array = {5, 3, 8, 4, 2};
+        int size = array.length;
+        
+        System.out.println("Array before sorting:");
+        printArray(array);
+        
+        bubbleSort(array, size);
+        
+        System.out.println("Array after sorting:");
+        printArray(array);
+    }
     
-    
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
 }
